1f1c3ba3a42720e08e19f0e5752b192a72d388e0
edwwn <40034470+edwwn@users.noreply.github.com>
2019-04-01 19:31:30 +0300

    onTap creation

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+      return new Material(
+        color: Colors.greenAccent,
+        child: new InkWell(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text("lets Quizzs", style: new TextStyle(color: Colors.white,fontSize: 50, fontWeight: FontWeight.bold ),),
+              new Text("Tap to Start", style: new TextStyle(color: Colors.blue, fontSize: 60, fontWeight: FontWeight.bold),)
+            ],
+          ),
+        ),
+      );
+  }
+
+}
