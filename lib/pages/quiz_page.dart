4b94bd71d4b04a34b4f1090feed3a86d1721343f
edwwn <40034470+edwwn@users.noreply.github.com>
2019-04-04 21:39:32 +0300

    up to the adding of the pi animation on container

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -4,32 +4,30 @@ import '../utils/question.dart';
 import '../utils/quiz.dart';
 
 import '../UI/answer_button.dart';
+import '../UI/question_text.dart';
+
+import '../UI/correct_wrong_overlay.dart';
 
 class QuizPage extends StatefulWidget{
     @override
     State createState() => new QuizPageState();
     }
-class QuizPageState extends State<QuizPage>{
+class QuizPageState extends State<QuizPage>{ 
   @override
   Widget build(BuildContext context){
     return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
         new Column(//THIS IS OUR MAIN PAGE
           children: <Widget>[
             new AnswerButton(true, () => print("You answered True")),
-            new Material(
-              color: Colors.white,
-              child: new Padding(
-               padding: new EdgeInsets.symmetric(vertical:20.0),
-                child: new Center(
-                  child: new Text("Statement 1: Githeri is Nice"),
-              ),
-             ),
-            ),
+            new QuestionText("Githeri is Nice", 1),
             new AnswerButton(false, () => print("You answered False")),
           ],
-        ) 
+        ),
+        new CorrectWrongOverlay(false)
       ], 
     );
   }
 }
+ 
